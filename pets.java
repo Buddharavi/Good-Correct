8069e43c1b3a865b7f548beadc9307c230f9ca23
Buddharavi <buddharavi302@gmail.com>
2016-06-18 17:10:49 +0530

    Add files via upload

---
+++ pets.java
@@ -0,0 +1,13 @@
+public class pets{
+	public static void main(String[] args){
+		String[] pets = {"dog","cat","parrot"};
+		int x=pets.length;
+		System.out.print("x is" +x);
+		int y=(int) 24.8;
+		System.out.println("x is" +y);
+		String s=pets[0];
+		s=s+""+"is a dog";
+		System.out.print("x is" +s);
+	}
+		
+}
