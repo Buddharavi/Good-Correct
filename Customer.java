24e48c353bdcc03a05e5adb3afc44cba8add3781
Buddharavi <buddharavi302@gmail.com>
2016-07-11 12:06:44 +0530

    Add files via upload

---
+++ Customer.java
@@ -0,0 +1,15 @@
+class Customer
+{
+int account_no;
+float balance_Amt;
+String name;
+int age;
+String address;
+void balance_inquiry()
+{
+}
+void fund_Transfer()
+{
+}
+public static void main(String args[]){}
+}
