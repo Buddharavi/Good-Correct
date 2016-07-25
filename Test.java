693072414c95ef0de86af52e1918e76de764d359
Buddharavi <buddharavi302@gmail.com>
2016-07-25 12:16:33 +0530

    Add files via upload

--- Test.java
+++ Test.java
@@ -3,25 +3,33 @@ class Example{
    void m1(){
       System.out.println("m1");
    }
-	/*void m2(){
-      System.out.println("m1 hi");
-}*/
-
 }
 class Sample extends Example{
-   
-   /*void m1(){
-      System.out.println("m2");
-}*/
 	void m2(){
       System.out.println("m2 bye");
 }
 }
 class Test{
+	static void m3(Object obj){
+if(obj instanceof Example)
+{
+Example e=(Example)obj;
+e.m1();
+}
+else if(obj instanceof Sample)
+{
+Sample s=(Sample)obj;
+s.m1();
+s.m2();
+}
+}
    public static void main(String args[]){
-	Example e=new Sample();
-	e.m1();
-	Sample s=(Sample)e;
-	s.m2();
+	Test.m3(new Example());
+	Test.m3(new Sample());
+	Test.m3(new Object());	
+	/*System.out.println(e instanceof Sample);
+	System.out.println(s instanceof Sample);
+	System.out.println(e instanceof Example);
+	System.out.println(s instanceof Example);*/
 }
 }
