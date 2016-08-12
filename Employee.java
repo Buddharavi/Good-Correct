e0a1083c2f14edbfaaacb4b8057397b6eedb96b8
Buddharavi <buddharavi302@gmail.com>
2016-08-12 13:00:46 +0530

    Add files via upload

--- Employee.java
+++ Employee.java
@@ -1,13 +1,50 @@
-class Employee
-{
- int eid;  // data member (or instance variable)
- String ename;  // data member (or instance variable)
- eid=101;
- ename="Hitesh";
- public static void main(String args[])
- { 
-  Employee e=new Employee(); // Creating an object of class Employee
-  System.out.println("Employee ID: "+e.eid);
-  System.out.println("Name: "+e.ename);
- }  
-} 
+/**
+ * Created by DhanaSM on 12-08-2016.
+ **/
+import java.io.*;
+import java.util.*;
+
+public abstract class Employee implements Comparable{
+    private String firstName;
+    private String lastName;
+    private String middleName;
+    private int salary;
+
+    public Employee(String firstName, String middleName, String lastName, int salary) {
+        this.firstName = firstName;
+        this.middleName = middleName;
+        this.lastName = lastName;
+        this.salary = salary;
+    }
+
+    //int avgSalary() {}
+    public int compareTO(Employee compareEmp){
+        int compareSal = compareEmp.getEmployeeSalary();
+        return this.getEmployeeSalary()-compareSal;
+    }
+    public String getEmployeeFirstName() {
+        return this.firstName;
+    }
+
+    public String getEmployeeMiddleName() {
+        return this.middleName;
+    }
+
+    public String getEmployeeLastName() {
+        return this.lastName;
+    }
+
+    public int getEmployeeSalary() {
+        return this.salary;
+    }
+}
+class Operations{
+    public static void main(String[] args){
+        ArrayList Employee=new ArrayList();
+        Employee.add(new Employee("ravi", "kanth", "Buddha", 20000));
+        Employee.add(new Employee("rav", "kant", "Buddh", 2000));
+        Employee.add(new Employee("ra","kan","Budd",2000));
+        Collections.sort(Employee);
+    }
+}
+
